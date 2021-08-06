4cbeb3a632a663398bb986b725e7e70cf8206b22
shivakumar007 <shiva0071998@gmail.com>
2021-08-06 17:20:55 +0530

    Code for empty string as input

---
+++ src/com/calculator/StringCalculator.java
@@ -0,0 +1,14 @@
+package com.calculator;
+
+public class StringCalculator {
+    public static int Add(String numbers){
+        if(numbers.equals(" ")||numbers.equals("")){
+            return 0;
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(Add(""));
+    }
+}
